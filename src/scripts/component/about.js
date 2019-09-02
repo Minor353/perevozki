2f9377bda68ea760ae2498ae9e57ab29c845efb5
Hennadii Nikitenko <vankert353@gmail.com>
2019-09-02 19:13:36 +0300

    Правки при наполнении контентом

--- src/scripts/component/about.js
+++ src/scripts/component/about.js
@@ -5,6 +5,8 @@ module.exports = function () {
     speed: 800,
     slidesToShow: 1,
     slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
     prevArrow: '.about_slider-prev',
     nextArrow: '.about_slider-next',
   });
--- src/scripts/component/priceTabs.js
+++ src/scripts/component/priceTabs.js
@@ -1,18 +1,22 @@
 module.exports = function () {
   const tabsBtn = document.querySelectorAll('.price_tabs-item');
   const tabsItem = document.querySelectorAll('.price_tabs-content');
+  const tabsImg = document.querySelectorAll('.price_content-img-content');
 
   tabsBtn[0].classList.add('price_tabs-item--active');
   tabsItem[0].classList.add('price_tabs-content--active');
+  tabsImg[0].classList.add('price_content-img-content--active');
 
   for (let i = 0; i < tabsBtn.length; i++) {
     tabsBtn[i].addEventListener('click', function () {
       for (let i = 0; i < tabsBtn.length; i++) {
         tabsBtn[i].classList.remove('price_tabs-item--active');
         tabsItem[i].classList.remove('price_tabs-content--active');
+        tabsImg[i].classList.remove('price_content-img-content--active');
       }
       tabsBtn[i].classList.add('price_tabs-item--active');
       tabsItem[i].classList.add('price_tabs-content--active');
+      tabsImg[i].classList.add('price_content-img-content--active');
     })
   }
 }
